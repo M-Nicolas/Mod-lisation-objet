6f4acb586f01e50085667ac778874930594e6317
Corentin <corentin.fouyaya@hotmail.fr>
2015-10-23 09:38:06 +0200

    update gitignore and modelRobot.vpp
    test is only for communication.

---
+++ test.cpp
@@ -0,0 +1,27 @@
+Robot {
+	void avancer(int x, int y){
+		currentState.avancer(x, y, cx, cy);
+	}
+	void saisir(Obj o){
+		saisir(Objet, this);
+	}
+};
+
+
+current_state{
+	saisir(Obj, Rob){
+	}
+	v_saisir(Obj, Rob){
+		// vrai méthode
+		Rob.setObjet(Obj);
+	}
+	avancer(int,int,int,int){}
+};
+
+aVideFacePlot{
+	saisir(Obj, Rob){
+		v_saisir(Obj, Rob);
+	}
+};
+
+fige{};
