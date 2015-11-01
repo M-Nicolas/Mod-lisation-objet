dc8b13140f79e1e59ee727181328221b815caede
M-Nicolas <meurgues.nicolas@gmail.com>
2015-11-01 11:09:44 +0100

    Modification et ajout de documentation
    Modification pour que le code C++ soit plus propre et ajout de
    documentation sur tout les membres de la classe Robot

--- test.cpp
+++ test.cpp
@@ -1,50 +1,56 @@
-﻿class Robot {
-private :
-	Object object;
-	Position position;
-	
+﻿//classe du Robot
+class Robot {
 public:
+	//Méthode permettant au robot d'avancer : modifie ses parametres de Position
 	void avancer(int x, int y){	
 		try{	
 			currentState.avancer();
-			Position.setX(x);
 		} catch(not_available_method_exception e) {
-
+			//TODO : réaction en cas d'utilisation de méthode non autorisé
 		}
 
 	}
-
+	//Méthode permettant au robot de saisir l'Object se trouvant devant lui
 	void saisir(Obj o){
 		try{
 			saisir(Object, this);
 		} catch(not_available_method_exception e) {
-
+			//TODO : réaction en cas d'utilisation de méthode non authorisé
 		}
 	}
+	//méthode retournant la position actuelle du robot
+	Position get_position() {
+		return this->position;
+	}
+
+private:
+	Object object; //objet tenu par le robot : peut être NULL s'il ne tient rien
+	Position position; //Position du robot dans son état actuel 
 };
 
+//Exception retournée quand le robot ne peux faire une action dans un étât donné
 class not_available_method_exception(){};
 
 //classe dont hériterons les différents états
-class current_state{
+class Current_state{
 public:
-	saisir(Obj, Rob){
-		throws not_available_method_exception();
+	saisir(Object obj, Robot rob){
+		throw not_available_method_exception();
 	}
-
-	v_saisir(Obj, Rob){
-		throws not_available_method_exception();
+	v_saisir(Object obj, Robot rob){
+		throw not_available_method_exception();
 	}
-
-	avancer(int,int,Pos){}	
+	avancer(int x, int y, Position pos,Robot rob){
+		throw not_available_method_exception();
+	}	
 };
 
-//TODO : modifier l'héritage pour qu'il corresponde à du C++
-class aVideFacePlot extends current_state{
-	saisir(Obj, Rob){
-		// vrai méthode
-		Rob.setObject(Obj);
+class aVideFacePlot extends Current_state{ //TODO : modifier l'héritage pour qu'il corresponde à du C++
+	saisir(Object obj, Robot rob){
+		rob.setObject(obj);
 	}
 };
 
-class fige extends current_state{};
+class fige extends Current_state{ //TODO : modifier l'héritage pour qu'il corresponde à du C++
+
+};
