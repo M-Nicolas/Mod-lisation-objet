55dfb53ad0fe5ad735c91e22eee4a7eaaf5083ba
Corentin <corentin.fouyaya@hotmail.fr>
2015-11-05 10:51:10 +0100

    premiers rajouts dans Robot.cpp
    Aucun changement d'état n'est géré. Quelques méthodes ont été en partie implémenté.

--- Robot.cpp
+++ Robot.cpp
@@ -1,31 +1,75 @@
-#include <Current_state.h>
+//classe du Robot 						XXX aucun changement d'état n'est fait.
 
-//classe du Robot
-class Robot {
-public:
-	//Méthode permettant au robot d'avancer : modifie ses parametres de Position
-	void avancer(int x, int y){	
-		try{	
-			currentState.avancer();
-		} catch(not_available_method_exception e) {
-			//TODO : réaction en cas d'utilisation de méthode non autorisé
-		}
+//Méthode permettant au robot d'avancer : modifie ses parametres de Position
+void Robot::avancer(int x, int y){	
+	try{	
+		_currentState.avancer();
+		_x = x;
+		_y = y;
+	} catch(not_available_method_exception e) {
+		//TODO : réaction en cas d'utilisation de méthode non autorisé
+	}
+}
 
+// Méthode permettant au robot de tourner : modifie sa direction
+void Robot::tourner(String direction){
+	try{
+		_currentState.tourner();
+		_direction = direction;
+	} catch(not_available_method_exception e) {
+		//TODO : réaction en cas d'utilisation de méthode non autorisé
 	}
-	//Méthode permettant au robot de saisir l'Object se trouvant devant lui
-	void saisir(Obj o){
-		try{
-			saisir(Object, this);
-		} catch(not_available_method_exception e) {
-			//TODO : réaction en cas d'utilisation de méthode non authorisé
-		}
-	}
-	//méthode retournant la position actuelle du robot
-	Position get_position() {
-		return this->position;
-	}
+}
 
-private:
-	Object object; //objet tenu par le robot : peut être NULL s'il ne tient rien
-	Position position; //Position du robot dans son état actuel 
-};
+//Méthode permettant au robot de saisir l'Objet se trouvant devant lui
+void Robot::saisir(Objet obj){
+	try{
+		_currentState.saisir();
+		_obj = obj;
+	} catch(not_available_method_exception e) {
+		//TODO : réaction en cas d'utilisation de méthode non authorisé
+	}
+}
+
+// Méthode permettant au robot de poser l'objet qu'il tient
+void Robot::poser(){
+	try{
+		_currentState.poser();
+		_obj = Objet();
+	} catch(not_available_method_exception e) {
+		//TODO : réaction en cas d'utilisation de méthode non authorisé
+	}
+}
+
+// Méthode retournant le poids de l'Objet que tient le robot
+int Robot::peser(){
+	try{
+		_currentState.peser();
+		return _obj.getPoids();
+	} catch(not_available_method_exception e) {
+		//TODO : réaction en cas d'utilisation de méthode non authorisé
+	}	
+}
+
+void Robot::rencontrerPlot(){
+	try{
+		_currentState.rencontrerPlot();
+	} catch(not_available_method_exception e) {
+		//TODO : réaction en cas d'utilisation de méthode non authorisé
+	}	
+}
+
+// Getter retournant la position actuelle du robot
+Position Robot::get_position() {
+	return this->position;
+}
+
+/* XXX corps d'une fonction basique
+(){
+	try{
+		_currentState.
+	} catch(not_available_method_exception e) {
+		//TODO : réaction en cas d'utilisation de méthode non authorisé
+	}	
+}
+*/
