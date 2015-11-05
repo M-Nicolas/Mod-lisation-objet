9c4c2ba0048533a8ddd5f0580f773879ffd54d21
Corentin <corentin.fouyaya@hotmail.fr>
2015-11-05 12:12:36 +0100

    Imlpementation de Plot

---
+++ datas/Plot.cpp
@@ -0,0 +1,7 @@
+Plot::Plot(int hauteur)
+: _hauteur(hauteur)
+{}
+
+int Plot::getHauteur(){
+	return _hauteur;
+}
---
+++ datas/Plot.h
@@ -0,0 +1,9 @@
+class Plot
+{
+private:
+	int _hauteur;
+public:
+	Plot(int = 0);
+
+	int getHauteur();
+};
