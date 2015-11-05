c4ff6216a387191e2b97763ba7fcd41abd26124f
M-Nicolas <meurgues.nicolas@gmail.com>
2015-11-05 10:46:57 +0100

    Correction de .h
    Correction du .h de aVideFacePlot.

--- AVideFacePlot.h
+++ AVideFacePlot.h
@@ -1,19 +1,7 @@
+//Classe aVideFacePlot qui correspont à l'etat du Robot lorsqu'il ne porte pas d'objet et qu'il se trouve face à un Plot
 class aVideFacePlot extends Current_state{ //TODO : modifier l'héritage pour qu'il corresponde à du C++
-	void saisir(Object obj, Robot rob){
-		rob.setObject(obj);
-	}
-
-	void tourner(String direction, Robot rob) {
-		switch {
-			case "N" : rob.setDirection("N");
-			case "E" : rob.setDirection("E");
-			case "S" : rob.setDirection("S");
-			case "W" : rob.setDirection("W");
-			default : throw bad_direction_exception();
-		}
-	}
-
-	int evaluerPlot(Object obj) {
-		return obj.getHauteur();
-	}
+public:
+	void saisir(Object obj, Robot rob);
+	void tourner(String direction, Robot rob);
+	int evaluerPlot(Object obj);
 };
