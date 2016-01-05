1819dd1afd41f8a7e175d21b4141b18db1dccf7b
Nicolas <meurgues.nicolas@gmail.com>
2016-01-05 22:06:28 +0100

    ajout des cout

--- 2_src/Robot.cpp
+++ 2_src/Robot.cpp
@@ -9,44 +9,55 @@ Robot::Robot()
 void Robot::avancer(int x, int y){	
 	_position.setX(x);
 	_position.setY(y);
+	cout << "le robot s'est positionné en (" << x << ", " << y << ")" << endl;
 }
 
 // Méthode permettant au robot de tourner : modifie sa direction
 void Robot::tourner(string direction){
 	_direction = direction;
+	cout << "le robot et maintenant dans la direction " << direction << "." << endl;
 }
 
 //Méthode permettant au robot de saisir l'Objet se trouvant devant lui
 void Robot::saisir(Objet obj){
 	_obj = obj;
+	cout << "l'objet à été saisis" << endl;
 }
 
 // Méthode permettant au robot de poser l'objet qu'il tient
 void Robot::poser(){
 	_obj = Objet();
+	cout << "l'objet a été posé" << endl;
 }
 
 // Méthode retournant le poids de l'Objet que tient le robot
 int Robot::peser(){
-	return _obj.getPoids();
+	int poids = _obj.getPoids();
+	cout << "l'objet pèse " << poids << " kg" << endl;
+	return poids; 
 }
 
 // Méthode permettant au Robot de découvrir le Plot plot
 void Robot::rencontrerPlot(Plot plot){
 	_plot = plot;
+	cout << "plot rencontré" << endl;
 }
 
 // Méthode premettant de mesurer la hauteur du plot rencontrer prècèdement
 int Robot::evaluerPlot(){
-	return _plot.getHauteur();
+	int hauteur = _plot.getHauteur();
+	cout << "le plot mesure " << hauteur << " cm de haut" << endl;
+	return hauteur;
 }
 
 // méthode permettant de mettre en pause le robot
 void Robot::figer(){
+	cout << "le robot est figé" << endl;
 }
 
 // méthode permettant au robot figé de repartir
 void Robot::repartir(){
+	cout << "le robot n'est plus figé" << endl;
 }
 
 void Robot::agit(){
